76650a6d3a98860839c71f1f88284bfbee259f53
Ilyalifsh <ilyalifsh@gmail.com>
2021-10-09 13:30:00 +0300

    first commit (Initial commit)

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
-		System.out.println("HW of lecturer");
+		System.out.println("There are following variants:");
 
 	}
 
